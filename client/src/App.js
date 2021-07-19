145f4052490ebe3c9ccae28300563d13e20df3a2
puthpiseth <puthpiseth_tun@yahoo.com>
2021-07-19 10:42:43 +0200

    Style error-email page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import ForgotPassword from './pages/Forgot-password';
+import ErrorEmail from './pages/Error-email';
 import './styles/pages/signup.scss'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
         <Route exact path='/signup' component={Signup}></Route>
         <Route exact path='/signin' component={Signin}></Route>
         <Route exact path='/forgot-password' component={ForgotPassword}></Route>
+        <Route exact path='/error-email' component={ErrorEmail}></Route>
       </Switch>
     </Router>
   );
