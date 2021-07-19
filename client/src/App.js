303b3b315bc1e57f506dcf405dbe56791bfbf83e
puthpiseth <puthpiseth_tun@yahoo.com>
2021-07-19 14:19:02 +0200

    Style reset-password page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import ForgotPassword from './pages/Forgot-password';
+import ResetPassword from './pages/Reset-password';
 import ErrorEmail from './pages/Error-email';
-import './styles/pages/signup.scss'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 
@@ -13,6 +13,7 @@ function App() {
         <Route exact path='/signup' component={Signup}></Route>
         <Route exact path='/signin' component={Signin}></Route>
         <Route exact path='/forgot-password' component={ForgotPassword}></Route>
+        <Route exact path='/reset-password' component={ResetPassword}></Route>
         <Route exact path='/error-email' component={ErrorEmail}></Route>
       </Switch>
     </Router>
