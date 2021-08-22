eaeeef58b2ef91c73007ea904d8d8a37447cc0e3
puthpiseth <puthpiseth_tun@yahoo.com>
2021-08-22 18:24:31 +0200

    Add private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,40 @@
+import React, {useContext} from 'react'
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
-import ForgotPassword from './pages/Forgot-password';
-import ResetPassword from './pages/Reset-password';
-import ErrorEmail from './pages/Error-email';
 import Profile from './pages/Profile';
 import PrivatePolicy from './pages/Private-policy'
 import UploadArticle from './pages/Upload_Article';
 import EditProfile from './pages/EditProfile';
 import Home from './pages/Home';
 import Footer from './components/Footer';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import AppContext from './store';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 
+const PrivateRoute = (props) => {
+
+  const authContext = useContext(AppContext)
+
+  if(localStorage.getItem('token')) {
+    return <Route component={props.component} exact path={props.path} />
+  } else {
+    return <Route render ={() => <Redirect to ='/' />} />
+  }
+}
 function App() {
+
   return (
-    <Router>
-      {/* <Navbar/> */}
-      <Switch>
-        <Route exact path='/signup' component={Signup} />
-        <Route exact path='/signin' component={Signin} />
-        <Route exact path ='/home' component ={Home} />
-        <Route exact path='/forgot-password' component={ForgotPassword} />
-        <Route exact path='/reset-password' component={ResetPassword} />
-        <Route exact path='/error-email' component={ErrorEmail} />
-        <Route exact path='/upload-article' component={UploadArticle} />
-        <Route exact path='/edit-profile' component={EditProfile} />
-        <Route exact path='/profile' component={Profile} />
-        <Route exact path='/private-policy' component={PrivatePolicy} />
-      </Switch>
-      <Footer/>
-    </Router>
+      <Router>
+        <Switch>
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/' component={Signin} />
+          <PrivateRoute path ='/home' component ={Home} />
+          <PrivateRoute path='/upload-article' component={UploadArticle} />
+          <PrivateRoute path='/edit-profile' component={EditProfile} />
+          <PrivateRoute path='/profile' component={Profile} />
+          <Route exact path='/private-policy' component={PrivatePolicy} />
+        </Switch>
+        <Footer/>
+      </Router>
   );
 }
 
---
+++ client/src/components/Delete_Article.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DeleteArticle() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DeleteArticle;
